fc805ecaf76481d3ea811768e6491bf3581f3c7a
AndresGuerrero <andres.guerrero@qcode.co>
2020-07-10 02:56:54 -0500

    implement unit test

---
+++ Api/Api.Test/Api.Domain/EmployeeServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Api.Domain.ConcreteEmployee;
+using Api.Domain.Factory;
+using Api.Domain.IService;
+using Api.Domain.Service;
+using Api.Repository.IRepository;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Test.Api.Domain
+{
+    public class EmployeeServiceTest
+    {
+        Mock<IEmployeeRepository> employeeRepository= new Mock<IEmployeeRepository>();
+        Mock<EmployeeFactory> factory = new Mock<EmployeeFactory>();
+
+        [Fact]
+        public async Task RetrieveEmployeeByIdOKAsync()
+        {
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            var jss = new JsonSerializer
+            {
+                ContractResolver = contractResolver,
+                Formatting = Formatting.Indented
+            };
+
+            employeeRepository.Setup(x =>  x.RetrieveEmployeeList()).ReturnsAsync(JArray.FromObject(Constants.EMPLOYEES, jss));
+            var service = new EmployeeService(employeeRepository.Object, factory.Object);
+            var response = await service.RetrieveEmployeeById(5);
+            Assert.IsType<HourlySalaryEmployee>(response.Data);
+        }
+
+    }
+}
---
+++ Api/Api.Test/Constants.cs
@@ -0,0 +1,35 @@
+﻿using Api.Domain.ConcreteEmployee;
+using Api.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Test
+{
+    public static class Constants
+    {
+        public static readonly List<Employee> EMPLOYEES = new List<Employee>
+        {
+            new HourlySalaryEmployee
+            {
+                Id = 5,
+                Name = "Guerrero",
+                ContractTypeName = "HourlySalaryEmployee",
+                RoleId = 1,
+                RoleName = "Administrator",
+                HourlySalary = 10000,
+                MonthlySalary = 3500000
+            },
+            new MonthlySalaryEmployee
+            {
+                Id = 2,
+                Name = "Andres",
+                ContractTypeName = "MonthlySalaryEmployee",
+                RoleId = 1,
+                RoleName = "Contractor",
+                HourlySalary = 12000,
+                MonthlySalary = 2500000
+            }
+        };
+    }
+}
